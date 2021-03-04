ac56a7baf3c01bf282b63508f21f1013047f7e42
Athosgpm <athos.gpm@gmail.com>
2021-03-04 03:01:21 -0300

    feat(repository):update repository

--- src/main/java/org/generation/BlogPessoal/repository/TemaRepository.java
+++ src/main/java/org/generation/BlogPessoal/repository/TemaRepository.java
@@ -6,6 +6,5 @@ import org.generation.BlogPessoal.model.Tema;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TemaRepository extends JpaRepository<Tema, Long> {
-
 	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
 }
